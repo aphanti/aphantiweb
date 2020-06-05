7d8d09fceb6e95b33d1b9acf5bfd7c9655fd3b2e
Yu <yuhaiyan@8c8590431df4.ant.amazon.com>
2020-06-05 11:03:07 -0400

    add ckeditor upload in blog

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from ckeditor_uploader import views as uploader_views
 
 app_name = 'blog'
 
@@ -8,5 +9,9 @@ urlpatterns = [
     path('create/', views.create_blog_view, name="create-blog"), 
     path('<int:pk>/', views.blogdetailview, name='blog-detail'), 
     path('<int:pk>/update/', views.update_blog_view, name="update-blog"), 
+    path('ckeditor/upload/', 
+        uploader_views.upload, name='ckeditor_upload'), 
+    path('ckeditor/browse/', 
+        uploader_views.browse, name='ckeditor_browse'), 
 ]
 
